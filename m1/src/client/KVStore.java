2677b52983b494a828f654b86f3a3f9f348cfb38
Fang  Yu Tong <yu.fang@mail.utoronto.ca>
2018-01-19 19:05:58 -0500

    kvstore connection

--- m1/src/client/KVStore.java
+++ m1/src/client/KVStore.java
@@ -1,26 +1,112 @@
 package client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
 import common.messages.KVMessage;
 
-public class KVStore implements KVCommInterface {
+public class KVStore extends Thread implements KVCommInterface {
 	/**
 	 * Initialize KVStore with address and port of KVServer
 	 * @param address the address of the KVServer
 	 * @param port the port of the KVServer
 	 */
+	
+	public String addr;
+	public int portnum;
+	private Socket clientSocket;
+	private OutputStream output;
+ 	private InputStream input;
+	private Logger logger = Logger.getRootLogger();
+	private boolean running;
+	
 	public KVStore(String address, int port) {
 		// TODO Auto-generated method stub
+		this.addr = address;
+		this.portnum = port;
 	}
 
 	@Override
-	public void connect() throws Exception {
+	public void connect() throws Exception {							//add try-catch exception handler
 		// TODO Auto-generated method stub
+		clientSocket = new Socket(addr, portnum);
+		setRunning(true);
+		logger.info("Connection established");
 	}
 
 	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
+		logger.info("try to close connection ...");
+		
+		try {
+			tearDownConnection();
+			
+		} catch (IOException ioe) {
+			logger.error("Unable to close connection!");
+		}
 	}
+	
+	public void run() {
+		try {
+			output = clientSocket.getOutputStream();
+			input = clientSocket.getInputStream();
+			
+			while(isRunning()) {
+				try {
+					KVMessage latestMsg = receiveMessage();
+					//TO DO implement handle received message 
+					//System.out.println(latestMsg.getMsg());
+					
+				} catch (IOException ioe) {
+					if(isRunning()) {
+						logger.error("Connection lost!");
+						try {
+							tearDownConnection();
+			
+						} catch (IOException e) {
+							logger.error("Unable to close connection!");
+						}
+					}
+				}				
+			}
+		} catch (IOException ioe) {
+			logger.error("Connection could not be established!");
+			
+		} finally {
+			if(isRunning()) {
+				disconnect();
+			}
+		}
+	}
+	
+	public boolean isRunning() {
+		return running;
+	}
+	
+	public void setRunning(boolean run) {
+		running = run;
+	}
+	
+	
+	private void tearDownConnection() throws IOException {
+		setRunning(false);
+		logger.info("tearing down the connection ...");
+		if (clientSocket != null) {
+			input.close();
+			output.close();
+			clientSocket.close();
+			clientSocket = null;
+			logger.info("connection closed!");
+		}
+	}
+
 
 	@Override
 	public KVMessage put(String key, String value) throws Exception {
@@ -33,4 +119,74 @@ public class KVStore implements KVCommInterface {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public void sendMessage(TextMessage msg) throws IOException {
+		byte[] msgBytes = msg.getMsgBytes();
+		output.write(msgBytes, 0, msgBytes.length);
+		output.flush();
+		logger.info("Send message:\t '" + msg.getMsg() + "'");
+    }
+	
+	
+	private KVMessage receiveMessage() throws IOException {
+		
+		int index = 0;
+		byte[] msgBytes = null, tmp = null;
+		byte[] bufferBytes = new byte[BUFFER_SIZE];
+		
+		/* read first char from stream */
+		byte read = (byte) input.read();	
+		boolean reading = true;
+		
+		while(read != 13 && reading) {/* carriage return */
+			/* if buffer filled, copy to msg array */
+			if(index == BUFFER_SIZE) {
+				if(msgBytes == null){
+					tmp = new byte[BUFFER_SIZE];
+					System.arraycopy(bufferBytes, 0, tmp, 0, BUFFER_SIZE);
+				} else {
+					tmp = new byte[msgBytes.length + BUFFER_SIZE];
+					System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+					System.arraycopy(bufferBytes, 0, tmp, msgBytes.length,
+							BUFFER_SIZE);
+				}
+
+				msgBytes = tmp;
+				bufferBytes = new byte[BUFFER_SIZE];
+				index = 0;
+			} 
+			
+			/* only read valid characters, i.e. letters and numbers */
+			if((read > 31 && read < 127)) {
+				bufferBytes[index] = read;
+				index++;
+			}
+			
+			/* stop reading is DROP_SIZE is reached */
+			if(msgBytes != null && msgBytes.length + index >= DROP_SIZE) {
+				reading = false;
+			}
+			
+			/* read next char from stream */
+			read = (byte) input.read();
+		}
+		
+		if(msgBytes == null){
+			tmp = new byte[index];
+			System.arraycopy(bufferBytes, 0, tmp, 0, index);
+		} else {
+			tmp = new byte[msgBytes.length + index];
+			System.arraycopy(msgBytes, 0, tmp, 0, msgBytes.length);
+			System.arraycopy(bufferBytes, 0, tmp, msgBytes.length, index);
+		}
+		
+		msgBytes = tmp;
+		
+		/* build final String */
+		TextMessage msg = new TextMessage(msgBytes);												//change to our implementation of serialization
+		logger.info("Receive message:\t '" + msg.getMsg() + "'");
+		return msg;
+    }
+ 	
 }
+
