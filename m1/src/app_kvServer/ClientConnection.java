3897f0dfa08d8e20c814b22d7cfb6f4b51bcbe5a
YONG AN LAI <yongan.lai@mail.utoronto.ca>
2018-01-23 21:16:49 -0500

    Server

--- m1/src/app_kvServer/ClientConnection.java
+++ m1/src/app_kvServer/ClientConnection.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import client.TextMessage;
+import common.messages.KVMessage;
+import common.messages.KVMessage.StatusType;
+import common.messages.KVMessageC;
 import org.apache.log4j.*;
 
 
@@ -41,29 +44,69 @@ public class ClientConnection implements Runnable {
 	 * Initializes and starts the client connection. 
 	 * Loops until the connection is closed or aborted by the client.
 	 */
-	public void run() {
+	public void run(){
 		try {
 			output = clientSocket.getOutputStream();
 			input = clientSocket.getInputStream();
 		
 			while(isOpen) {
+				KVMessageC message = new KVMessageC();
 				try {
 					TextMessage latestMsg = receiveMessage();
+					message.StrToKVM(latestMsg.getMsg());
+					String sKey = message.getKey();
+					String sValue = "";
+					String sRet = "";
+					if(message.getStatus() == StatusType.PUT)
+					{
+						boolean bOld = kvs.inStorage(sKey);
+						boolean bDelete = (message.getValue().length() == 0);
+						if(!bOld && bDelete)
+							sRet = "8 " + sKey;
+						else
+							kvs.putKV(sKey, message.getValue());
+						
+						int nStatus = -1;
+						if(bOld && bDelete)
+							nStatus = 7;
+						else if(bOld && !bDelete)
+							nStatus = 5;
+						else
+							nStatus = 4;
+						sRet = Integer.toString(nStatus) + " " + sKey + " " + message.getValue();
+							
+					}
+					else if(message.getStatus() == StatusType.GET)
+					{
+						sValue = kvs.getKV(sKey);
+						if(sValue.length() == 0)
+						{
+							sRet = "1 " + sKey;
+						}
+						else
+							sRet = "2 " + sKey + " " + sValue;
+					}
 					
-					sendMessage(latestMsg);
+					
+					sendMessage(new TextMessage(sRet));
 					
 				/* connection either terminated by the client or lost due to 
 				 * network problems*/	
 				} catch (IOException ioe) {
 					logger.error("Error! Connection lost!");
 					isOpen = false;
+				} 
+				catch (Exception e)
+				{
+					logger.error("Error!", e);
 				}				
 			}
 			
 		} catch (IOException ioe) {
 			logger.error("Error! Connection could not be established!", ioe);
 			
-		} finally {
+		}
+		finally {
 			
 			try {
 				if (clientSocket != null) {
