f8e5e6cfdbb41eaac1917797c4d9ff3ad85370b7
YONG AN LAI <yongan.lai@mail.utoronto.ca>
2018-03-03 15:47:39 -0500

    Merged KVClient, KVStore and KVMessage

--- m2/src/client/KVStore.java
+++ m2/src/client/KVStore.java
@@ -1,42 +1,188 @@
 package client;
 
-import common.KVMessage;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+import Utilities.Utilities;
+import org.apache.log4j.Logger;
+
+import client.ClientSocketListenerInterface.SocketStatus;
+
+import common.messages.KVMessage;
+import common.messages.KVMessageC;
+import common.messages.KVMessage.StatusType;
 
 public class KVStore implements KVCommInterface {
 	/**
 	 * Initialize KVStore with address and port of KVServer
-	 * @param address the address of the KVServer
-	 * @param port the port of the KVServer
+	 * 
+	 * @param address
+	 *            the address of the KVServer
+	 * @param port
+	 *            the port of the KVServer
 	 */
+
+	public String addr;
+	public int portnum;
+	private Socket clientSocket;
+	private Logger logger = Logger.getRootLogger();
+	private boolean running;
+	private Set<ClientSocketListener> listeners;
+	private ClientSocketListener listener;
+	private MessageStream stream;
+	private KVMessageC kvmsg;
+	private boolean bReceived;
+	private Utilities util;
+
 	public KVStore(String address, int port) {
 		// TODO Auto-generated method stub
+		addr = address;
+		portnum = port;
+		listeners = new HashSet<ClientSocketListener>();
+		bReceived = false;
+		kvmsg = new KVMessageC();
+		util = new Utilities();
 	}
 
 	@Override
 	public void connect() throws Exception {
 		// TODO Auto-generated method stub
+		clientSocket = new Socket(addr, portnum);
+		listener = new ClientSocketListener(addr, portnum);
+		stream = new MessageStream(clientSocket.getOutputStream(),
+				clientSocket.getInputStream());
+		addListener(listener);
+		setRunning(true);
+		logger.info("Connection established");
+		start();
 	}
 
 	@Override
 	public void disconnect() {
 		// TODO Auto-generated method stub
+		logger.info("try to close connection ...");
+		System.out.println("try to close connection ...");
+		try {
+			tearDownConnection();
+			for (ClientSocketListener listener : listeners) {
+				listener.handleStatus(SocketStatus.DISCONNECTED);
+			}
+		} catch (IOException ioe) {
+			logger.error("Unable to close connection!");
+		}
+	}
+
+	public void run() {
+		try {
+			while (isRunning()) {
+				try {
+					TextMessage latestMsg = stream.receiveMessage();
+					for (ClientSocketListener listener : listeners) {
+						kvmsg.StrToKVM(listener.handleNewMessage(latestMsg));
+						bReceived = true;
+					}
+				} catch (IOException ioe) {
+					if (isRunning()) {
+						logger.error("Connection lost!");
+						try {
+							tearDownConnection();
+							for (ClientSocketListener listener : listeners) {
+								listener.handleStatus(SocketStatus.CONNECTION_LOST);
+							}
+						} catch (IOException e) {
+							logger.error("Unable to close connection!");
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+		} finally {
+			if (isRunning()) {
+				disconnect();
+			}
+		}
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+	
+	public void setRunning(boolean run) {
+		running = run;
+	}
+
+	private void tearDownConnection() throws IOException {
+		setRunning(false);
+		logger.info("tearing down the connection ...");
+		if (clientSocket != null) {
+			clientSocket.close();
+			clientSocket = null;
+		}
+		if (stream != null) {
+			stream.streamClose();
+			stream = null;
+		}
+		logger.info("connection closed!");
+	}
+
+	private void addListener(ClientSocketListener listener) {
+		listeners.add(listener);
 	}
 
 	@Override
 	public boolean isConnected() {
 		// TODO Auto-generated method stub
-		return false;
+		return running;
 	}
 
 	@Override
 	public KVMessage put(String key, String value) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		// turn string into textmsg obj
+		// send it to server
+		try {
+			if (util.InvaildKey(key)) {
+				KVMessageC message = new KVMessageC(key, value,
+						StatusType.PUT_ERROR);
+				return message;
+			}
+			if (value == null)
+				value = "";
+			String msg = "3 " + key + " " + value;
+			stream.sendMessage(new TextMessage(msg));
+		} catch (IOException e) {
+			logger.error("Client> " + "Error! " + "Unable to send message!");
+			disconnect();
+		}
+
+		while (true) {
+			Thread.sleep(1);
+			if (bReceived) {
+				bReceived = false;
+				break;
+			}
+		}
+		return kvmsg;
 	}
 
 	@Override
 	public KVMessage get(String key) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		try {
+
+			String msg = "0 " + key;
+			stream.sendMessage(new TextMessage(msg));
+		} catch (IOException e) {
+			logger.error("Client> " + "Error! " + "Unable to send message!");
+			disconnect();
+		}
+		while (true) {
+			Thread.sleep(1);
+			if (bReceived) {
+				bReceived = false;
+				break;
+			}
+		}
+		return kvmsg;
 	}
 }
---
+++ m2/src/common/KVMessageC.java
@@ -0,0 +1,48 @@
+package common.messages;
+
+import common.messages.KVMessage;
+import common.messages.KVMessage.StatusType;
+
+public class KVMessageC implements KVMessage{
+	
+	private String key;
+	private String value;
+	private StatusType status;
+
+	public KVMessageC() {
+		key = null;
+		value = null;
+		status = null;
+	}
+	
+	public KVMessageC(String sKey, String sValue, StatusType statusType) {
+		key = sKey;
+		value = sValue;
+		status = statusType;
+	}
+
+	public String getKey(){
+		return key;
+	}
+	
+	public String getValue(){
+		return value;
+	}
+	
+	
+	public StatusType getStatus(){
+		return status;
+	}
+	
+	public void StrToKVM (String str){
+		String[] splited = str.split(" ");
+		status = StatusType.values()[Integer.parseInt(splited[0])];
+		key = splited[1];
+		int nKeyLength = key.length();
+		if(3 + nKeyLength >= str.length())														//e.g 0 key value 
+			value = "";
+		else
+			value = str.substring(3 + nKeyLength);
+	}
+
+}
