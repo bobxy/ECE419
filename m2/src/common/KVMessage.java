678e91994928ff73030f57a023a25417ae862673
YONG AN LAI <yongan.lai@mail.utoronto.ca>
2018-03-05 15:01:28 -0500

    New message types

--- m2/src/common/KVMessage.java
+++ m2/src/common/KVMessage.java
@@ -5,19 +5,19 @@ import ecs.IECSNode;
 public interface KVMessage {
 
 	public enum StatusType {
-		GET, 			/* Get - request */
-		GET_ERROR, 		/* requested tuple (i.e. value) not found */
-		GET_SUCCESS, 	/* requested tuple (i.e. value) found */
-		PUT, 			/* Put - request */
-		PUT_SUCCESS, 	/* Put - request successful, tuple inserted */
-		PUT_UPDATE, 	/* Put - request successful, i.e. value updated */
-		PUT_ERROR, 		/* Put - request not successful */
-		DELETE_SUCCESS, /* Delete - request successful */
-		DELETE_ERROR, 	/* Delete - request successful */
+		GET, 			/* Get - request 0*/
+		GET_ERROR, 		/* requested tuple (i.e. value) not found 1*/
+		GET_SUCCESS, 	/* requested tuple (i.e. value) found 2*/
+		PUT, 			/* Put - request 3*/
+		PUT_SUCCESS, 	/* Put - request successful, tuple inserted 4*/
+		PUT_UPDATE, 	/* Put - request successful, i.e. value updated 5*/
+		PUT_ERROR, 		/* Put - request not successful 6*/
+		DELETE_SUCCESS, /* Delete - request successful 7*/
+		DELETE_ERROR, 	/* Delete - request successful 8*/
 
-		SERVER_STOPPED,         /* Server is stopped, no requests are processed */
-		SERVER_WRITE_LOCK,      /* Server locked for out, only get possible */
-		SERVER_NOT_RESPONSIBLE  /* Request not successful, server not responsible for key */
+		SERVER_STOPPED,         /* Server is stopped, no requests are processed 9*/
+		SERVER_WRITE_LOCK,      /* Server locked for out, only get possible 10*/
+		SERVER_NOT_RESPONSIBLE  /* Request not successful, server not responsible for key 11*/
 	}
 
 	/**
